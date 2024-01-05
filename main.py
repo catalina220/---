759572c605f87f47a22e3a0335a5f102a45332e2
Kate Samarin <99908400+catalina220@users.noreply.github.com>
2024-01-05 15:54:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication
-from PyQt5.uic import loadUi
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtCore import Qt
 import random
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-
-class CircleDrawer(QMainWindow):
+class Form(QMainWindow):
     def __init__(self):
-        super(CircleDrawer, self).__init__()
-        loadUi('UI.ui', self)  # Загружаем интерфейс из файла UI.ui
-        self.setWindowTitle('Circle Drawer')
-        self.button_draw_circle.clicked.connect(self.draw_circle)
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.button.clicked.connect(self.drawCircles)
+        self.draw = False
 
-    def draw_circle(self):
-        diameter = random.randint(10, 100)
-        painter = QPainter(self)
-        painter.setPen(QColor(Qt.yellow))
-        painter.setBrush(QColor(Qt.yellow))
-        painter.drawEllipse(100, 100, diameter, diameter)
+    def drawCircles(self):
+        self.draw = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.draw:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setBrush(QColor(255, 255, 0))
+            r = random.randint(10, 100)
+            qp.drawEllipse(random.randint(10, 200), random.randint(10, 200), r, r)
+            qp.end()
+            self.draw = False
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = CircleDrawer()
-    window.show()
-    sys.exit(app.exec_())
+    f = Form()
+    f.show()
+    sys.exit(app.exec())
