76ab7492acd4a10f8288ddd00842b1b33e73d931
Kate Samarin <99908400+catalina220@users.noreply.github.com>
2024-01-07 16:43:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ class Form(QMainWindow):
         if self.draw:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 255, 0))
+            r = random.randint(0, 255)
+            g = random.randint(0, 255)
+            b = random.randint(0, 255)
+            qp.setBrush(QColor(r, g, b))
             r = random.randint(10, 100)
             qp.drawEllipse(random.randint(10, 200), random.randint(10, 200), r, r)
             qp.end()
