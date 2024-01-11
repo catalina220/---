433fd6fc1c590d36aa9d57c61ddcc4d101547129
Kate Samarin <99908400+catalina220@users.noreply.github.com>
2024-01-11 11:56:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
 import sys
-import random
-from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt
+from random import randint
 
-class Form(QMainWindow):
+
+class CircleWidget(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.button.clicked.connect(self.drawCircles)
-        self.draw = False
+        self.initUI()
+        self.circles = []
 
-    def drawCircles(self):
-        self.draw = True
-        self.repaint()
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Random Circles')
+        self.button = QPushButton('Add Circle', self)
+        self.button.clicked.connect(self.addCircle)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
 
     def paintEvent(self, event):
-        if self.draw:
-            qp = QPainter()
-            qp.begin(self)
-            r = random.randint(0, 255)
-            g = random.randint(0, 255)
-            b = random.randint(0, 255)
+        qp = QPainter()
+        qp.begin(self)
+        self.drawCircles(qp)
+        qp.end()
+
+    def addCircle(self):
+        diameter = randint(10, 100)
+
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        
+        x = randint(0, self.width() - diameter)
+        y = randint(0, self.height() - diameter)
+
+        self.circles.append((x, y, diameter, r, g, b))
+        self.update()
+
+    def drawCircles(self, qp):
+        for circle in self.circles:
+            x, y, diameter, r, g, b = circle
+
             qp.setBrush(QColor(r, g, b))
-            r = random.randint(10, 100)
-            qp.drawEllipse(random.randint(10, 200), random.randint(10, 200), r, r)
-            qp.end()
-            self.draw = False
+            qp.drawEllipse(x, y, diameter, diameter)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    f = Form()
-    f.show()
-    sys.exit(app.exec())
+    circleWidget = CircleWidget()
+    circleWidget.show()
+    sys.exit(app.exec_())
